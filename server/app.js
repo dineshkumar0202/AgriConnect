5a6ee1c81c562548479c5c6cb061d06fd88feea4
dineshkumar0202 <aesthetic.dinesh02@gmail.com>
2025-10-14 22:41:51 +0530

    connected MongoDB

--- server/app.js
+++ server/app.js
@@ -3,32 +3,36 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/routeAuth.js";
-import productRoutes from "./routes/routeProduct.js";
-import commentRoutes from "./routes/routeComment.js";
-import newsRoutes from "./routes/routeNews.js";
+// import authRoutes from "./routes/auth.js";
+// import productRoutes from "./routes/product.js";
+// import commandeRoutes from "./routes/commande.js";
+// import newsRoutes from "./routes/news.js";
+// import interestRoutes from "./routes/interest.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/news", newsRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use(".api/products", productRoutes);
+// app.use("/api/commandes", commandeRoutes);
+// app.use("/api/news", newsRoutes);
+// app.use("/api/interests", interestRoutes);
 
-// Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log("✅ MongoDB connected");
-  app.listen(process.env.PORT, () =>
-    console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
-  );
-})
-.catch(err => console.error("❌ MongoDB connection error:", err));
+// connect to MongoDB
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+  })
+  .catch(err => {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
+  });
