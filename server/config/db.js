2edcbe191b3aaa3d9c535518944e3636aff6077f
dineshkumar0202 <aesthetic.dinesh02@gmail.com>
2026-02-27 15:05:35 +0530

    Fix all Erorrs

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
   try {
     const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/agri_marketplace";
     const conn = await mongoose.connect(uri);
-    console.log("MongoDB connected🌱:", conn.connection.host);
+    console.log("MongoDB connected🌱");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
